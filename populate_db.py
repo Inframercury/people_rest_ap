d7d13379babc8cbae6ec2b7eee5a6656914a14a0
Philipp Lukyanov <inframercury@gmail.com>
2017-09-29 14:40:16 +0700

    Fix requirements and app.py files

--- populate_db.py
+++ populate_db.py
@@ -1,10 +1,26 @@
 import sqlite3
-conn = sqlite3.connect('people_rest_db.sqlite')
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Person, Base
 
-c = conn.cursor()
-# c.execute('CREATE TABLE person (id INTEGER PRIMARY KEY ASC, name varchar(250) NOT NULL )')
-#c.execute('INSERT INTO person VALUES (2,"Phil",15000,"01-01-2010")')
-#c.execute('UPDATE person SET name="{}" WHERE id={}'.format('Philipp','1'))
-c.execute('SELECT sqlite_version()')
-conn.commit()
-conn.close()
+
+engine = create_engine('sqlite:///people_rest_db.sqlite')
+Session = sessionmaker()
+Session.configure(bind=engine)
+Base.metadata.create_all(engine)
+
+
+#conn = sqlite3.connect('people_rest_db.sqlite')
+s = Session()
+
+people = [
+    Person(id=1,name='Vasya',salary=15000.00,birthday='01-01-1989'),
+    Person(id=2,name='Kolya',salary=30000.00,birthday='01-01-1960'),
+    Person(id=3,name='Fedya',salary=40000.00,birthday='01-01-1989'),
+    Person(id=4,name='Max',salary=20000.00,birthday='01-01-1990'),
+    Person(id=5,name='Masha',salary=17000.00,birthday='01-01-19876'),
+]
+
+s.bulk_save_objects(people)
+s.commit()
+s.close()
